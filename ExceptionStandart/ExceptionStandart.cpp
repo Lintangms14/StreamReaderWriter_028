0f210a637832f4b64591ce6f4e3d6b33ed298d0a
Lintangms14 <merayusukmalintang@gmail.com>
2023-06-21 15:52:31 +0700

    catch

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -15,4 +15,12 @@ int main()
 		cout << data.at(5) << endl;
 		// Memanggil array element ke 5
 	}
+	catch (exception& e) {
+		// Perangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		/*akan di eksekusi karena array data hanya memiliki 3 element*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/*Penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+	return 0;
 }
